bfa474ef8f5c119b553d88f36356fe17bb13ac06
Chris Baldwin <cbaldwin@shaw.ca>
2016-12-04 19:19:34 -0800

    Added comments.

--- src/com/fincad/thermometer/ThermometerFactory.java
+++ src/com/fincad/thermometer/ThermometerFactory.java
@@ -6,10 +6,22 @@ package com.fincad.thermometer;
  * @author Chris Baldwin
  */
 public class ThermometerFactory {
+   /**
+    * Create a Thermometer instance that reads from the given TemperatureSource.
+    * 
+    * @param source the source this temperature instance should read from
+    */
    public static Thermometer createThermometer(TemperatureSource source) {
        return new ThermometerImpl(source);
    } 
    
+   /**
+    * Construct a Thermometer instance that reads from the given TemperatureSource
+    * and set the default temperature scale to the given value.
+    * 
+    * @param source the source this temperature instance should read from
+    * @param defaultScale the default temperature scale to use
+    */
    public static Thermometer createThermometer(TemperatureSource source, TemperatureScale scale) {
        return new ThermometerImpl(source, scale);
    }
